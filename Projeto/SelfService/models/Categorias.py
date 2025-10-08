2e42804ad915e0aef82dd8d28ffa70d5064cd288
Galessss <marcus.balbino.2001@gmail.com>
2025-10-08 16:05:33 -0300

    agora vai

---
+++ Projeto/SelfService/models/Categorias.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+
+class ItemCardapio(models.Model):
+    CATEGORIAS = (
+        ('comida', 'Comida'),
+        ('bebida', 'Bebida'),
+    )
+    nome = models.CharField(max_length=100)
+    descricao = models.TextField(blank=True)
+    preco = models.DecimalField(max_digits=6, decimal_places=2)
+    categoria = models.CharField(max_length=10, choices=CATEGORIAS, default='comida')
